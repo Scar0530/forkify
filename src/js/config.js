77f54f9610be77a5e425365ee283a62fb0369040
Scar0530 <mamukaxalichashvili5@gmail.com>
2022-11-30 21:30:47 +0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SECONDS = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6f96990b-6e7d-4139-85cd-45e5d5080d13';
+export const MODAL_CLOSE_SEC = 2.5;
